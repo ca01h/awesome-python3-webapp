59e11969b95ae19c123a1534eeab65ccd7d06111
caoyihuai2 <cc511965738@gmail.com>
2017-01-18 10:10:26 +0800

    编写MVC

--- www/coroweb.py
+++ www/coroweb.py
@@ -11,6 +11,7 @@ from aiohttp import web
 
 from apis import APIError
 
+
 def get(path):
     '''
     Define decorator @get('/path')
@@ -23,6 +24,7 @@ def get(path):
         wrapper.__route__ = path
         return wrapper
     return decorator
+
 
 def post(path):
     '''
@@ -37,6 +39,7 @@ def post(path):
         return wrapper
     return decorator
 
+
 def get_required_kw_args(fn):
     args = []
     params = inspect.signature(fn).parameters
@@ -44,6 +47,7 @@ def get_required_kw_args(fn):
         if param.kind == inspect.Parameter.KEYWORD_ONLY and param.default == inspect.Parameter.empty:
             args.append(name)
     return tuple(args)
+
 
 def get_named_kw_args(fn):
     args = []
@@ -53,17 +57,20 @@ def get_named_kw_args(fn):
             args.append(name)
     return tuple(args)
 
+
 def has_named_kw_args(fn):
     params = inspect.signature(fn).parameters
     for name, param in params.items():
         if param.kind == inspect.Parameter.KEYWORD_ONLY:
             return True
 
+
 def has_var_kw_arg(fn):
     params = inspect.signature(fn).parameters
     for name, param in params.items():
         if param.kind == inspect.Parameter.VAR_KEYWORD:
             return True
+
 
 def has_request_arg(fn):
     sig = inspect.signature(fn)
@@ -76,6 +83,7 @@ def has_request_arg(fn):
         if found and (param.kind != inspect.Parameter.VAR_POSITIONAL and param.kind != inspect.Parameter.KEYWORD_ONLY and param.kind != inspect.Parameter.VAR_KEYWORD):
             raise ValueError('request parameter must be the last named parameter in function: %s%s' % (fn.__name__, str(sig)))
     return found
+
 
 class RequestHandler(object):
 
